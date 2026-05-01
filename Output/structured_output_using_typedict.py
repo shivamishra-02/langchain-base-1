7fc825e70d1283d64221f10da0efaaf75a9e6e68
Shivam mishra <shivammishra993187@gmail.com>
2026-05-02 00:02:32 +0530

    how to get the structured output using the typedict while generating response from LLMs

--- Output/structured_output_using_typedict.py
+++ Output/structured_output_using_typedict.py
@@ -1,21 +1,20 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 from typing import TypedDict, Annotated, Optional, Literal
-from pydantic import BaseModel, Field
 
 load_dotenv()
 
 model = ChatOpenAI()
 
 # schema
-class Review(BaseModel):
+class Review(TypedDict):
 
-    key_themes: list[str] = Field(description="Write down all the key themes discussed in the review in a list")
-    summary: str = Field(description="A brief summary of the review")
-    sentiment: Literal["pos", "neg"] = Field(description="Return sentiment of the review either negative, positive or neutral")
-    pros: Optional[list[str]] = Field(default=None, description="Write down all the pros inside a list")
-    cons: Optional[list[str]] = Field(default=None, description="Write down all the cons inside a list")
-    name: Optional[str] = Field(default=None, description="Write the name of the reviewer")
+    key_themes: Annotated[list[str], "Write down all the key themes discussed in the review in a list"]
+    summary: Annotated[str, "A brief summary of the review"]
+    sentiment: Annotated[Literal["pos", "neg"], "Return sentiment of the review either negative, positive or neutral"]
+    pros: Annotated[Optional[list[str]], "Write down all the pros inside a list"]
+    cons: Annotated[Optional[list[str]], "Write down all the cons inside a list"]
+    name: Annotated[Optional[str], "Write the name of the reviewer"]
     
 
 structured_model = model.with_structured_output(Review)
@@ -32,7 +31,7 @@ Stunning 200MP camera with incredible zoom capabilities
 Long battery life with fast charging
 S-Pen support is unique and useful
                                  
-Review by Shivam Mishra
+Review by Nitish Singh
 """)
 
-print(result)
+print(result['name'])
